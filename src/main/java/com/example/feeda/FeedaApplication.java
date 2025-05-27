ecc5f3e226813da8d2feca9ed1c4c1e0a0b0838d
leeuihyun <meta212121@naver.com>
2025-05-27 20:25:16 +0900

    feat : SpringBoot 초기설정 도메인별 패키지 설정 및 create-issue-branch 설정

---
+++ src/main/java/com/example/feeda/FeedaApplication.java
@@ -0,0 +1,13 @@
+package com.example.feeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/feeda/FeedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
