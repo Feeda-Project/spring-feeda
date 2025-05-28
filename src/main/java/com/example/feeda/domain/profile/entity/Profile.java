7a1990313bf033bdf2187dd0491f7dc5f215e53a
Che0807 <elpin0428@naver.com>
2025-05-28 15:54:14 +0900

    "feat: Entity 생성"

--- src/main/java/com/example/feeda/domain/profile/entity/Profile.java
+++ src/main/java/com/example/feeda/domain/profile/entity/Profile.java
@@ -1,4 +1,19 @@
 package com.example.feeda.domain.profile.entity;
 
-public class Profile {
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Profile extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long accountId;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    private Date birth;
+
+    private String bio;
 }
