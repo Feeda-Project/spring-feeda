efa45eecfb5ec9cdf7bae2f8c3660affc93b27c4
🍆 가지 <gajicoding@gmail.com>
2025-05-30 12:04:03 +0900

    Revert "[#14]프로필 기능 구현 연관 관계 적용 및 오류 수정"

--- src/main/java/com/example/feeda/exception/TokenNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.feeda.exception;
-
-public class TokenNotFoundException extends RuntimeException {
-    public TokenNotFoundException(String message) {
-        super(message);
-    }
-}
