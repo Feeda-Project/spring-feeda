b17c25a0d60157edfaf35e4e65b83f81047174c7
leeuihyun <meta212121@naver.com>
2025-06-02 15:54:34 +0900

    feat : 팔로잉한 사람들의 게시글 조회 작성 In을 사용하여 게시글 조회

--- src/main/java/com/example/feeda/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/feeda/domain/post/repository/PostRepository.java
@@ -1,14 +1,14 @@
 package com.example.feeda.domain.post.repository;
 
 import com.example.feeda.domain.post.entity.Post;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Page<Post> findAllByTitleContaining(String title, Pageable pageable);
 
+    Page<Post> findAllByProfile_IdIn(List<Long> followingProfileIds, Pageable pageable);
 }
